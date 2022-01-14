066507a4d366e7b748b7d5ccf8cbc602f9b3454d
MrNewaz <mr.saifnewaz@gmail.com>
2022-01-14 23:57:44 +0600

    model test case added

--- user/tests/test_child_urls.py
+++ user/tests/test_child_urls.py
@@ -2,70 +2,7 @@
 # from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
-from .models import *
-
-
-# Api testing for Parent user
-class ParentAPITests(APITestCase):
-
-    # Setting up parent user
-    def setUp(self):
-        self.parent = Parent.objects.create(
-            first_name="Saif",
-            last_name="Newaz",
-            street="Bou Bari Goli",
-            city="Dhaka",
-            state="Dhaka",
-            zip_code=1206
-        )
-
-    # Get List of Parent Users
-    def test_get_parents_list(self):
-        client = APIClient()
-        response = client.get("/user/parent/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # Create a Parent User
-    def test_parent_user_create(self):
-        data = {
-            "first_name": "Saif",
-            "last_name": "Newaz",
-            "street": "Bou Bari Goli",
-            "city": "Dhaka",
-            "state": "Dhaka",
-            "zip_code": 1206
-        }
-
-        client = APIClient()
-        response = client.post("/user/parent/", data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    # Upadate a Parent User
-    def test_parent_user_update(self):
-
-        data = {
-            "first_name": "Saif",
-            "last_name": "Newaz",
-            "street": "House no 177, North Vashantek",
-            "city": "Dhaka",
-            "state": "Dhaka",
-            "zip_code": 1206
-        }
-
-        client = APIClient()
-        self.parent.refresh_from_db()
-        response = client.put("/user/parent/"+str(self.parent.id)+"/", data)
-        self.assertEqual(response.status_code,
-                         status.HTTP_200_OK)
-
-    # Delete a Parent User
-    def test_parent_user_delete(self):
-
-        client = APIClient()
-        self.parent.refresh_from_db()
-        response = client.delete("/user/parent/"+str(self.parent.id)+"/")
-        self.assertEqual(response.status_code,
-                         status.HTTP_204_NO_CONTENT)
+from ..models import *
 
 
 # Api Testing for Child user
--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,16 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
+
+# Parent Route
+# Get & Post /parent
+# Put & Delete /parent/<pk>
 router.register('parent', ParentViewSet)
+
+
+# Parent Route
+# Get & Post /child
+# Put & Delete /child/<pk>
 router.register('child', ChildViewSet)
 
 urlpatterns = router.urls
