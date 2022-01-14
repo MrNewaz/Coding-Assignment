98a1c33b00fca602a930fb09fdeb2f6ef4474725
MrNewaz <mr.saifnewaz@gmail.com>
2022-01-14 19:15:06 +0600

    init

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 admin.site.site_header = 'Coding Assignment'
 
 urlpatterns = [
+    path('', views.homepage),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
 ]
