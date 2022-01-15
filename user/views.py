20b672e7398935966d29d9b6480e2c9c6aad8b78
MrNewaz <mr.saifnewaz@gmail.com>
2022-01-15 15:55:02 +0600

    Excluded patch

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from http.client import responses
+from rest_framework import status
 
 from rest_framework.viewsets import ModelViewSet
 from .serializers import *
@@ -10,8 +11,20 @@ class ParentViewSet(ModelViewSet):
     queryset = Parent.objects.all().order_by('id')
     serializer_class = ParentSerializer
 
+    """
+    Folowing the instructions from the assignment, 
+    only alowing the GET, POST, PUT and DELETE methods. (Get is allowed so that the routes can be tested)
+    """
+    http_method_names = ['get', 'post', 'put', 'delete']
+
 
 # Child ViewSet
 class ChildViewSet(ModelViewSet):
     queryset = Child.objects.all().order_by('id')
     serializer_class = ChildSerializer
+
+    """
+    Folowing the instructions from the assignment, 
+    only alowing the GET, POST, PUT and DELETE methods. (Get is allowed so that the routes can be tested)
+    """
+    http_method_names = ['get', 'post', 'put', 'delete']
