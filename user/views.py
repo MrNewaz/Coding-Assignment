61658c57b9271d0e6d889eea0035aad67ed442bd
MrNewaz <mr.saifnewaz@gmail.com>
2022-01-15 15:58:44 +0600

    done

--- user/views.py
+++ user/views.py
@@ -11,20 +11,11 @@ class ParentViewSet(ModelViewSet):
     queryset = Parent.objects.all().order_by('id')
     serializer_class = ParentSerializer
 
-    """
-    Folowing the instructions from the assignment, 
-    only alowing the GET, POST, PUT and DELETE methods. (Get is allowed so that the routes can be tested)
-    """
-    http_method_names = ['get', 'post', 'put', 'delete']
+    # Not Sure if I should exclude partial update or not.
+    # http_method_names = ['get', 'post', 'put', 'delete']
 
 
 # Child ViewSet
 class ChildViewSet(ModelViewSet):
     queryset = Child.objects.all().order_by('id')
     serializer_class = ChildSerializer
-
-    """
-    Folowing the instructions from the assignment, 
-    only alowing the GET, POST, PUT and DELETE methods. (Get is allowed so that the routes can be tested)
-    """
-    http_method_names = ['get', 'post', 'put', 'delete']
